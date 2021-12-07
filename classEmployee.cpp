4e2e01628b2fc64f19ea85de2a018867d5fc055d
pratik-karande <karandepratik17@outlook.com>
2021-12-07 10:58:07 +0530

    new class program

---
+++ classEmployee.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+class Employee{
+    private:
+    int id;
+    string name;
+    float salary;
+
+    public:
+    void insert(int i,string n,int sal){
+        id=i;
+        name=n;
+        salary=sal;
+
+    }
+
+    void display(){
+        cout<<id<<" "<<name<<" "<<"Rs."<<salary<<endl;
+
+    }
+
+};
+int main(){
+    Employee e1;
+    Employee e2;
+    e1.insert(200,"Pratik",40000 );
+    e2.insert(201,"Pradnyesh",50000);
+    e1.display();
+    e2.display();
+
+    return 0;
+}
